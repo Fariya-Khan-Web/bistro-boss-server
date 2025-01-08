1337578536a65b9afe2d87c228259ac480e532a0
Fariya <fariya.webdev@gmail.com>
2025-01-08 06:33:30 +0600

    feat: update user to admin

--- index.js
+++ index.js
@@ -35,6 +35,12 @@ async function run() {
 
 
         // user related api
+
+        app.get('/users', async(req, res)=>{
+            const result = await userCollection.find().toArray()
+            res.send(result)
+        })
+
         app.post('/users', async (req, res) => {
             const user = req.body
 
@@ -45,6 +51,25 @@ async function run() {
             }
 
             const result = await userCollection.insertOne(user)
+            res.send(result)
+        })
+
+        app.patch('/dashboard/admin/:id', async(req, res)=>{
+            const id = req.params.id
+            const filter = {_id: new ObjectId(id)}
+            const updatedUser = {
+                $set: {
+                    role: 'admin'
+                }
+            }
+            const result = await userCollection.updateOne(filter, updatedUser)
+            res.send(result)
+        })
+
+        app.delete('/users/:id', async(req, res)=>{
+            const id = req.params.id
+            const query = { _id: new ObjectId(id)}
+            const result = await userCollection.deleteOne(query)
             res.send(result)
         })
 
