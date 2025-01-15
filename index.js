42d4c19803d65bc7aa2be388b6c51709c1b6721a
Fariya <fariya.webdev@gmail.com>
2025-01-16 02:19:18 +0600

    feat: item add, delete, update

--- index.js
+++ index.js
@@ -9,32 +9,6 @@ const port = process.env.PORT || 2000;
 // middlewere
 app.use(cors())
 app.use(express.json())
-const verifyToken = (req, res, next) => {
-    // console.log('inside verify token', req.headers)
-
-    if (!req.headers.authorization) {
-        return res.status(401).send({ message: 'unauthorized access' })
-    }
-    const token = req.headers.authorization.split(' ')[1];
-    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
-        if (err) {
-            return res.status(401).send({ message: 'unauthorized access' })
-        }
-        req.decoded = decoded
-        console.log({ decoded })
-        next();
-    })
-}
-const verifyAdmin = async (req, res, next) => {
-    const email = req.decoded.email
-    const query = { email: email }
-    const user = await userCollection.findOne(query)
-    const isAdmin = user?.role === 'admin'
-    if (!isAdmin) {
-        return res.status(403).send({ message: 'forbidden access' })
-    }
-    next();
-}
 
 
 
@@ -62,10 +36,42 @@ async function run() {
         const cartCollection = database.collection('cartItems')
 
 
+
+        const verifyToken = (req, res, next) => {
+            // console.log('inside verify token', req.headers)
+
+            if (!req.headers.authorization) {
+                return res.status(401).send({ message: 'unauthorized access' })
+            }
+            const token = req.headers.authorization.split(' ')[1];
+            jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+                if (err) {
+                    return res.status(401).send({ message: 'unauthorized access' })
+                }
+                req.decoded = decoded
+                console.log({ decoded })
+                next();
+            })
+        }
+
+
+        const verifyAdmin = async (req, res, next) => {
+            const email = req.decoded.email
+            const query = { email: email }
+            const user = await userCollection.findOne(query)
+            const isAdmin = user?.role === 'admin'
+            if (!isAdmin) {
+                return res.status(403).send({ message: 'forbidden access' })
+            }
+            next();
+        }
+
+
+
         // jwt related token
         app.post('/jwt', async (req, res) => {
             const user = req.body
-            const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1h' })
+            const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '6  h' })
             res.send({ token })
         })
 
@@ -78,7 +84,7 @@ async function run() {
             res.send(result)
         })
 
-        
+
         app.post('/users', verifyToken, async (req, res) => {
             const user = req.body
 
@@ -136,6 +142,45 @@ async function run() {
             res.send(result)
         })
 
+        app.get('/menu/:id', async (req, res) => {
+            const id = req.params.id
+            const query = { _id: id }
+            const result = await menuCollection.findOne(query)
+            res.send(result)
+        })
+
+        app.put('/menu/:id', async (req, res) => {
+            const id = req.params.id
+            const item = req.body
+            const filter = { _id: id }
+            const updateDocument = {
+                $set: {
+                    name: item.name,
+                    category: item.category,
+                    price: item.price,
+                    recipe: item.recipe,
+                   
+                },
+            };
+            const options = { upsert: true }
+            const result = await menuCollection.updateOne(filter, updateDocument, options)
+            res.send(result)
+        })
+
+        app.post('/menu', verifyToken, verifyAdmin, async (req, res) => {
+            const menuItem = req.body
+            const result = await menuCollection.insertOne(menuItem)
+            res.send(result)
+
+        })
+
+        app.delete('/menu/:id', verifyToken, verifyAdmin, async (req, res) => {
+            const id = req.params.id
+            const query = { _id: new ObjectId(id) || id }
+            const result = await menuCollection.deleteOne(query)
+            res.send(result)
+        })
+
 
         // reviews on homepage
         app.get('/reviews', async (req, res) => {
@@ -145,8 +190,14 @@ async function run() {
 
 
         // cart related API
+
         app.get('/cartitems', async (req, res) => {
-            const email = req.query.email
+            const result = await cartCollection.find().toArray()
+            res.send(result)
+        })
+
+        app.get('/cartitems/:email', async (req, res) => {
+            const email = req.params.email
             const query = { email: email }
             const result = await cartCollection.find(query).toArray()
             res.send(result)
@@ -158,7 +209,7 @@ async function run() {
             res.send(result)
         })
 
-        app.delete('/cartitems/:id', async (req, res) => {
+        app.delete('/cartitem/:id', async (req, res) => {
             const id = req.params.id
             const query = { _id: new ObjectId(id) }
             const result = await cartCollection.deleteOne(query)
